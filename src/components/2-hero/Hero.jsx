4ee212658a3b1504ce102979df73d6309d8c4f4f
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-10-20 18:35:51 +0300

    Edit Hero

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -12,21 +12,25 @@ const Hero = () => {
           <div className="icon-verified"></div>
         </div>
         <h2 className="title-description">
-          Cross-Platform Mobile Application Developer.
+          Cross-Platform Mobile Application Developer
         </h2>
         <p className="sub-title">
-          I am a Highly-Motivated Jr. Mobile App Developer with a proven record
-          of creating highly-received and commercially successful applications.
-          My expertise lies in building user-friendly apps and collaborating
-          effectively within teams. I am constantly seeking opportunities to
-          enhance and refine existing apps, ensuring they captivate users and
-          stand the test of time.
+          I am a Junior Mobile App Developer who is passionate about creating
+          apps that people love. I have a track record of building successful
+          and well-received applications. My skills are focused on crafting
+          user-friendly apps and working effectively as part of a team. I am
+          always on the lookout for opportunities to improve and update existing
+          apps, making sure they continue to engage users and remain relevant
         </p>
         <div className="all-icons flex">
-          <div className="icon icon-twitter"></div>
-          <div className="icon icon-instagram"></div>
+          <div className="icon icon-x"></div>
+          <div className="icon icon-instagram1"></div>
           <div className="icon icon-github"></div>
-          <div className="icon icon-linkedin"></div>
+          <div className="icon icon-linkedin1"></div>
+          <div className="icon icon-whatsapp1"></div>
+          <div className="icon icon-upwork"></div>
+          <div className="icon icon-microsoftoutlook"></div>
+          <div className="icon icon-gmail"></div>
         </div>
       </div>
       <div className="right-section animation border">ANIMATIONS</div>
