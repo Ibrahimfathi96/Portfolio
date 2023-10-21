caba4148a4ea1bfa86d26026015b89522611698f
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-10-21 06:27:44 +0300

    Main Done

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -10,30 +10,35 @@ const Main = () => {
         <button>node & express</button>
         <button>HTML & CSS</button>
       </section>
-      <section className="right-section card">
-        <article className="card">
-          <img width={266} src="./1.jpg" alt="" />
+      <section className="right-section flex">
+        {["aa", "bb", "cc", 1, 7].map((item) => {
+          return (
+            <article key={item} className="card">
+              <img width={266} src="./1.jpg" alt="" />
 
-          <div style={{ width: "266px" }} className="box">
-            <h1 className="title">Project Title</h1>
-            <p className="sub-title">
-              lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Doloremque necessitatibus quidem, voluptatem quibusdam, adipisci
-              eum doloribus quia, voluptate quas quod? Lorem ipsum dolor sit
-              amet consectetur adipisicing elit. Doloremque necessitatibus.
-            </p>
-            <div className="flex all-icons">
-              <div style={{ gap: "11px" }} className="flex">
-                <div className="icon icon-link"></div>
-                <div className="icon icon-github"></div>
+              <div style={{ width: "266px" }} className="box">
+                <h1 className="title">Project Title</h1>
+                <p className="sub-title">
+                  lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque necessitatibus quidem, voluptatem quibusdam,
+                  adipisci eum doloribus quia, voluptate quas quod? Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit. Doloremque
+                  necessitatibus.
+                </p>
+                <div className="flex all-icons">
+                  <div style={{ gap: "11px" }} className="flex">
+                    <div className="icon icon-link"></div>
+                    <div className="icon icon-github"></div>
+                  </div>
+                  <a className="link flex" href="">
+                    more
+                    <span className="icon-arrow-right"></span>
+                  </a>
+                </div>
               </div>
-              <a className="link flex" href="">
-                more
-                <span className="icon-arrow-right"></span>
-              </a>
-            </div>
-          </div>
-        </article>
+            </article>
+          );
+        })}
       </section>
     </main>
   );
